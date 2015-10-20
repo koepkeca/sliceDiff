a425031d3a8464203d7ee07a9b14e33a9d9d965f
root <koepkeca@gmail.com>
2015-10-19 19:18:04 -0500

    Ran go fmt on the source files

--- sliceDiff.go
+++ sliceDiff.go
@@ -1,11 +1,11 @@
 // Package sliceDiff provides functions to determine the difference
 // between two slices. There are tests and limited benchmarks for this package.
 //
-// THIS IS INTENDED ONLY FOR "SMALL" SLICES. 
+// THIS IS INTENDED ONLY FOR "SMALL" SLICES.
 //
 // Using this package on slices > 30,000 entries can get slow.. I would
 // suggest benchmarking the number / type of slices you intend to use
-// in sliceDiff_test.go *before* using on slices with a large number 
+// in sliceDiff_test.go *before* using on slices with a large number
 // of entries.
 //
 // Use of this source code is governed by a BSD-style license
@@ -26,7 +26,7 @@ func Int64SliceDiff(sliceOne []int64, sliceTwo []int64) []int64 {
 				}
 			}
 			if !found {
-				diff = append(diff,s1)
+				diff = append(diff, s1)
 			}
 		}
 		if i == 0 {
@@ -39,27 +39,26 @@ func Int64SliceDiff(sliceOne []int64, sliceTwo []int64) []int64 {
 // Uint64SliceDiff returns the uint64 values that are not in
 // both source slices
 func Uint64SliceDiff(sliceOne []uint64, sliceTwo []uint64) []uint64 {
-    var diff []uint64
-    for i := 0; i < 2; i++ {
-        for _, s1 := range sliceOne {
-            found := false
-            for _, s2 := range sliceTwo {
-                if s1 == s2 {
-                    found = true
-                    break
-                }
-            }
-            if !found {
-                diff = append(diff,s1)
-            }
-        }
-        if i == 0 {
-            sliceOne, sliceTwo = sliceTwo, sliceOne
-        }
-    }
-    return diff
+	var diff []uint64
+	for i := 0; i < 2; i++ {
+		for _, s1 := range sliceOne {
+			found := false
+			for _, s2 := range sliceTwo {
+				if s1 == s2 {
+					found = true
+					break
+				}
+			}
+			if !found {
+				diff = append(diff, s1)
+			}
+		}
+		if i == 0 {
+			sliceOne, sliceTwo = sliceTwo, sliceOne
+		}
+	}
+	return diff
 }
-
 
 // Int32SliceDiff returns the int32 values that are not in
 // both soruce slices
@@ -75,7 +74,7 @@ func Int32SliceDiff(sliceOne []int32, sliceTwo []int32) []int32 {
 				}
 			}
 			if !found {
-				diff = append(diff,s1)
+				diff = append(diff, s1)
 			}
 		}
 		if i == 0 {
@@ -88,121 +87,121 @@ func Int32SliceDiff(sliceOne []int32, sliceTwo []int32) []int32 {
 // Uint32SliceDiff returns the uint32 values that are not in
 // both soruce slices
 func Uint32SliceDiff(sliceOne []uint32, sliceTwo []uint32) []uint32 {
-    var diff []uint32
-    for i := 0; i < 2; i++ {
-        for _, s1 := range sliceOne {
-            found := false
-            for _, s2 := range sliceTwo {
-                if s1 == s2 {
-                    found = true
-                    break
-                }
-            }
-            if !found {
-                diff = append(diff,s1)
-            }
-        }
-        if i == 0 {
-            sliceOne, sliceTwo = sliceTwo, sliceOne
-        }
-    }
-    return diff
+	var diff []uint32
+	for i := 0; i < 2; i++ {
+		for _, s1 := range sliceOne {
+			found := false
+			for _, s2 := range sliceTwo {
+				if s1 == s2 {
+					found = true
+					break
+				}
+			}
+			if !found {
+				diff = append(diff, s1)
+			}
+		}
+		if i == 0 {
+			sliceOne, sliceTwo = sliceTwo, sliceOne
+		}
+	}
+	return diff
 }
 
 // Int16SliceDiff returns the int16 values that are not in
 // both soruce slices
 func Int16SliceDiff(sliceOne []int16, sliceTwo []int16) []int16 {
-    var diff []int16
-    for i := 0; i < 2; i++ {
-        for _, s1 := range sliceOne {
-            found := false
-            for _, s2 := range sliceTwo {
-                if s1 == s2 {
-                    found = true
-                    break
-                }
-            }
-            if !found {
-                diff = append(diff,s1)
-            }
-        }
-        if i == 0 {
-            sliceOne, sliceTwo = sliceTwo, sliceOne
-        }
-    }
-    return diff
+	var diff []int16
+	for i := 0; i < 2; i++ {
+		for _, s1 := range sliceOne {
+			found := false
+			for _, s2 := range sliceTwo {
+				if s1 == s2 {
+					found = true
+					break
+				}
+			}
+			if !found {
+				diff = append(diff, s1)
+			}
+		}
+		if i == 0 {
+			sliceOne, sliceTwo = sliceTwo, sliceOne
+		}
+	}
+	return diff
 }
 
 // Uint16SliceDiff returns the uint16 values that are not in
 // both soruce slices
 func Uint16SliceDiff(sliceOne []uint16, sliceTwo []uint16) []uint16 {
-    var diff []uint16
-    for i := 0; i < 2; i++ {
-        for _, s1 := range sliceOne {
-            found := false
-            for _, s2 := range sliceTwo {
-                if s1 == s2 {
-                    found = true
-                    break
-                }
-            }
-            if !found {
-                diff = append(diff,s1)
-            }
-        }
-        if i == 0 {
-            sliceOne, sliceTwo = sliceTwo, sliceOne
-        }
-    }
-    return diff
+	var diff []uint16
+	for i := 0; i < 2; i++ {
+		for _, s1 := range sliceOne {
+			found := false
+			for _, s2 := range sliceTwo {
+				if s1 == s2 {
+					found = true
+					break
+				}
+			}
+			if !found {
+				diff = append(diff, s1)
+			}
+		}
+		if i == 0 {
+			sliceOne, sliceTwo = sliceTwo, sliceOne
+		}
+	}
+	return diff
 }
 
 // Int8SliceDiff returns the int8 values that are not in
 // both soruce slices
 func Int8SliceDiff(sliceOne []int8, sliceTwo []int8) []int8 {
-    var diff []int8
-    for i := 0; i < 2; i++ {
-        for _, s1 := range sliceOne {
-            found := false
-            for _, s2 := range sliceTwo {
-                if s1 == s2 {
-                    found = true
-                    break
-                }
-            }
-            if !found {
-                diff = append(diff,s1)
-            }
-        }
-        if i == 0 {
-            sliceOne, sliceTwo = sliceTwo, sliceOne
-        }
-    }
-    return diff
+	var diff []int8
+	for i := 0; i < 2; i++ {
+		for _, s1 := range sliceOne {
+			found := false
+			for _, s2 := range sliceTwo {
+				if s1 == s2 {
+					found = true
+					break
+				}
+			}
+			if !found {
+				diff = append(diff, s1)
+			}
+		}
+		if i == 0 {
+			sliceOne, sliceTwo = sliceTwo, sliceOne
+		}
+	}
+	return diff
 }
 
 // Uint8SliceDiff returns the uint8 values that are not in
 // both soruce slices
 func Uint8SliceDiff(sliceOne []uint8, sliceTwo []uint8) []uint8 {
-    var diff []uint8
-    for i := 0; i < 2; i++ {
-        for _, s1 := range sliceOne {
-            found := false
-            for _, s2 := range sliceTwo {
-                if s1 == s2 {
-                    found = true
-                    break
-                }
-            }
-            if !found {
-                diff = append(diff,s1)
-            }
-        }
-        if i == 0 {
-            sliceOne, sliceTwo = sliceTwo, sliceOne
-        }
-    }
-    return diff
+	var diff []uint8
+	for i := 0; i < 2; i++ {
+		for _, s1 := range sliceOne {
+			found := false
+			for _, s2 := range sliceTwo {
+				if s1 == s2 {
+					found = true
+					break
+				}
+			}
+			if !found {
+				diff = append(diff, s1)
+			}
+		}
+		if i == 0 {
+			sliceOne, sliceTwo = sliceTwo, sliceOne
+		}
+	}
+	return diff
 }
 
 // StringSliceDiff returns the string values that are not in
@@ -219,7 +218,7 @@ func StringSliceDiff(sliceOne []string, sliceTwo []string) []string {
 				}
 			}
 			if !found {
-				diff = append(diff,s1)
+				diff = append(diff, s1)
 			}
 		}
 		if i == 0 {
--- sliceDiff_test.go
+++ sliceDiff_test.go
@@ -3,196 +3,194 @@ package sliceDiff
 import "testing"
 
 const (
-	MSliceAMax = 256
-	MSliceAStep = 2
-	MSliceBMax = 768
-	MSliceBStep = 8
-	LSliceAMax = 4096
-	LSliceAStep = 2
-	LSliceBMax = 8192
-	LSliceBStep = 4
-	XLSliceAMax = 16384
-	XLSliceAStep = 2
-	XLSliceBMax = 7000
-	XLSliceBStep = 4
-	XXLSliceAMax = 32768
+	MSliceAMax    = 256
+	MSliceAStep   = 2
+	MSliceBMax    = 768
+	MSliceBStep   = 8
+	LSliceAMax    = 4096
+	LSliceAStep   = 2
+	LSliceBMax    = 8192
+	LSliceBStep   = 4
+	XLSliceAMax   = 16384
+	XLSliceAStep  = 2
+	XLSliceBMax   = 7000
+	XLSliceBStep  = 4
+	XXLSliceAMax  = 32768
 	XXLSliceAStep = 4
-	XXLSliceBMax = 65535
+	XXLSliceBMax  = 65535
 	XXLSliceBStep = 2
 )
 
-// GenerateInt64Slice generates random slices of int64's from 0 to s 
+// GenerateInt64Slice generates random slices of int64's from 0 to s
 // incrementing in incSize steps
 func GenerateInt64Slice(s int64, incSize int64) []int64 {
 	var n int64
-    var testSlice []int64
+	var testSlice []int64
 	for n = 0; n < s; n += incSize {
-		testSlice = append(testSlice,n)
+		testSlice = append(testSlice, n)
 	}
-	return(testSlice)
+	return (testSlice)
 }
 
 func GenerateUint64Slice(s uint64, incSize uint64) []uint64 {
 	var n uint64
 	var testSlice []uint64
 	for n = 0; n < s; n += incSize {
-		testSlice = append(testSlice,n)
+		testSlice = append(testSlice, n)
 	}
-	return(testSlice)
+	return (testSlice)
 }
 
 func BenchmarkUint64Diff_S(b *testing.B) {
-    testOne := []int64{1,2,3,4,5,6,7,8,9}
-    testTwo := []int64{1,2,3,5,6,7,8,10}
-    for i := 0; i < b.N; i++ {
-        Int64SliceDiff(testOne,testTwo)
-    }
+	testOne := []int64{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	testTwo := []int64{1, 2, 3, 5, 6, 7, 8, 10}
+	for i := 0; i < b.N; i++ {
+		Int64SliceDiff(testOne, testTwo)
+	}
 }
 
 func BenchmarkUint64Diff_M(b *testing.B) {
-    testOne := GenerateUint64Slice(MSliceAMax,MSliceAStep)
-	testTwo := GenerateUint64Slice(MSliceBMax,MSliceBStep)
-    for i := 0; i < b.N; i++ {
-        Uint64SliceDiff(testOne,testTwo)
-    }
+	testOne := GenerateUint64Slice(MSliceAMax, MSliceAStep)
+	testTwo := GenerateUint64Slice(MSliceBMax, MSliceBStep)
+	for i := 0; i < b.N; i++ {
+		Uint64SliceDiff(testOne, testTwo)
+	}
 }
 
 func BenchmarkInt64Diff_XXL(b *testing.B) {
-    testThree := GenerateInt64Slice(32768,4)
-    testFour := GenerateInt64Slice(65535,2)
-    for i := 0; i < b.N; i++ {
-		Int64SliceDiff(testThree,testFour)
+	testThree := GenerateInt64Slice(32768, 4)
+	testFour := GenerateInt64Slice(65535, 2)
+	for i := 0; i < b.N; i++ {
+		Int64SliceDiff(testThree, testFour)
 	}
 }
 
 func BenchmarkInt64Diff_S(b *testing.B) {
-	testOne := []int64{1,2,3,4,5,6,7,8,9}
-	testTwo := []int64{1,2,3,5,6,7,8,10}
-    for i := 0; i < b.N; i++ {
-		Int64SliceDiff(testOne,testTwo)
+	testOne := []int64{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	testTwo := []int64{1, 2, 3, 5, 6, 7, 8, 10}
+	for i := 0; i < b.N; i++ {
+		Int64SliceDiff(testOne, testTwo)
 	}
 }
 
 func BenchmarkInt64Diff_M(b *testing.B) {
-	testOne := GenerateInt64Slice(256,1)
-	testTwo := GenerateInt64Slice(768,6)
+	testOne := GenerateInt64Slice(256, 1)
+	testTwo := GenerateInt64Slice(768, 6)
 	for i := 0; i < b.N; i++ {
-		Int64SliceDiff(testOne,testTwo)
+		Int64SliceDiff(testOne, testTwo)
 	}
 }
 
 func BenchmarkInt64Diff_L(b *testing.B) {
-	testOne := GenerateInt64Slice(4096,2)
-	testTwo := GenerateInt64Slice(8192,4)
+	testOne := GenerateInt64Slice(4096, 2)
+	testTwo := GenerateInt64Slice(8192, 4)
 	for i := 0; i < b.N; i++ {
-		Int64SliceDiff(testOne,testTwo)
+		Int64SliceDiff(testOne, testTwo)
 	}
 }
 func BenchmarkInt64Diff_XL(b *testing.B) {
-	testOne := GenerateInt64Slice(16384,2)
-	testTwo := GenerateInt64Slice(7000,4)
+	testOne := GenerateInt64Slice(16384, 2)
+	testTwo := GenerateInt64Slice(7000, 4)
 	for i := 0; i < b.N; i++ {
-		Int64SliceDiff(testOne,testTwo)
+		Int64SliceDiff(testOne, testTwo)
 	}
 }
 
 func BenchmarkStringDiff_S(b *testing.B) {
-	testOne := []string {"example","one","two","three","four","five"}
-	testTwo := []string {"one","four","five"}
+	testOne := []string{"example", "one", "two", "three", "four", "five"}
+	testTwo := []string{"one", "four", "five"}
 	for i := 0; i < b.N; i++ {
-		StringSliceDiff(testOne,testTwo)
+		StringSliceDiff(testOne, testTwo)
 	}
 }
 
 func TestInt64Diff_Basic(t *testing.T) {
-	testOne := []int64 {1,2,3,5,6,7}
-	testTwo := []int64 {1,2,3,4,5,6,7,8}
-    diff := Int64SliceDiff(testOne,testTwo)
+	testOne := []int64{1, 2, 3, 5, 6, 7}
+	testTwo := []int64{1, 2, 3, 4, 5, 6, 7, 8}
+	diff := Int64SliceDiff(testOne, testTwo)
 	diffLen := len(diff)
-    if (diffLen != 2) {
+	if diffLen != 2 {
 		t.Fatal("Basic Int64 Test failed.")
 	}
 }
 
 func TestStringDiff_Basic(t *testing.T) {
-	testOne := []string {"example","test","one","two","three"}
-	testTwo := []string {"example","test","one","three"}
+	testOne := []string{"example", "test", "one", "two", "three"}
+	testTwo := []string{"example", "test", "one", "three"}
 
-    diff := StringSliceDiff(testOne,testTwo)
+	diff := StringSliceDiff(testOne, testTwo)
 	diffLen := len(diff)
-	if (diffLen != 1) {
+	if diffLen != 1 {
 		t.Fatal("Basic String Test Failed")
 	}
 }
 
 func TestInt32Diff_Basic(t *testing.T) {
-	testOne := []int32 {120,35,228,67,9,0,32760}
-	testTwo := []int32 {120,35,228,67,32760}
-    diff := Int32SliceDiff(testOne,testTwo)
-    diffLen := len(diff)
-	if (diffLen != 2) {
+	testOne := []int32{120, 35, 228, 67, 9, 0, 32760}
+	testTwo := []int32{120, 35, 228, 67, 32760}
+	diff := Int32SliceDiff(testOne, testTwo)
+	diffLen := len(diff)
+	if diffLen != 2 {
 		t.Fatal("Basic Int32 Test failed.")
 	}
 }
 
 func TestUint32Diff_Basic(t *testing.T) {
-    testOne := []uint32 {120,35,228,67,9,0,32760}
-    testTwo := []uint32 {120,35,228,67,32760}
-    diff := Uint32SliceDiff(testOne,testTwo)
-    diffLen := len(diff)
-    if (diffLen != 2) {
-        t.Fatal("Basic Uint32 Test failed.")
-    }
+	testOne := []uint32{120, 35, 228, 67, 9, 0, 32760}
+	testTwo := []uint32{120, 35, 228, 67, 32760}
+	diff := Uint32SliceDiff(testOne, testTwo)
+	diffLen := len(diff)
+	if diffLen != 2 {
+		t.Fatal("Basic Uint32 Test failed.")
+	}
 }
 
 func TestUint64Diff_Basic(t *testing.T) {
-    testOne := []uint64 {120,35,228,67,9,0,32760}
-    testTwo := []uint64 {120,35,228,67,32760}
-    diff := Uint64SliceDiff(testOne,testTwo)
-    diffLen := len(diff)
-    if (diffLen != 2) {
-        t.Fatal("Basic Uint64 Test failed.")
-    }
+	testOne := []uint64{120, 35, 228, 67, 9, 0, 32760}
+	testTwo := []uint64{120, 35, 228, 67, 32760}
+	diff := Uint64SliceDiff(testOne, testTwo)
+	diffLen := len(diff)
+	if diffLen != 2 {
+		t.Fatal("Basic Uint64 Test failed.")
+	}
 }
 
 func TestUint16_Basic(t *testing.T) {
-	testOne := []uint16 {2,4,6,7,8,10}
-	testTwo := []uint16 {2,4,6,8,10,13}
-	diff := Uint16SliceDiff(testOne,testTwo)
+	testOne := []uint16{2, 4, 6, 7, 8, 10}
+	testTwo := []uint16{2, 4, 6, 8, 10, 13}
+	diff := Uint16SliceDiff(testOne, testTwo)
 	diffLen := len(diff)
-	if (diffLen != 2) {
+	if diffLen != 2 {
 		t.Fatal("Basic Int16 Test failed.")
 	}
 }
 
 func TestInt16_Basic(t *testing.T) {
-    testOne := []int16 {2,4,6,7,8,10}
-    testTwo := []int16 {2,4,6,8,10,13}
-    diff := Int16SliceDiff(testOne,testTwo)
-    diffLen := len(diff)
-    if (diffLen != 2) {
-        t.Fatal("Basic Int16 Test failed.")
-    }
+	testOne := []int16{2, 4, 6, 7, 8, 10}
+	testTwo := []int16{2, 4, 6, 8, 10, 13}
+	diff := Int16SliceDiff(testOne, testTwo)
+	diffLen := len(diff)
+	if diffLen != 2 {
+		t.Fatal("Basic Int16 Test failed.")
+	}
 }
 
 func TestUint8_Basic(t *testing.T) {
-    testOne := []uint8 {2,4,6,7,8,10}
-    testTwo := []uint8 {2,4,6,8,10,13}
-    diff := Uint8SliceDiff(testOne,testTwo)
-    diffLen := len(diff)
-    if (diffLen != 2) {
-        t.Fatal("Basic Uint8 Test failed.")
-    }
+	testOne := []uint8{2, 4, 6, 7, 8, 10}
+	testTwo := []uint8{2, 4, 6, 8, 10, 13}
+	diff := Uint8SliceDiff(testOne, testTwo)
+	diffLen := len(diff)
+	if diffLen != 2 {
+		t.Fatal("Basic Uint8 Test failed.")
+	}
 }
 
 func TestInt8_Basic(t *testing.T) {
-    testOne := []int8 {2,4,6,7,8,10}
-    testTwo := []int8 {2,4,6,8,10,13}
-    diff := Int8SliceDiff(testOne,testTwo)
-    diffLen := len(diff)
-    if (diffLen != 2) {
-        t.Fatal("Basic Int8 Test failed.")
-    }
+	testOne := []int8{2, 4, 6, 7, 8, 10}
+	testTwo := []int8{2, 4, 6, 8, 10, 13}
+	diff := Int8SliceDiff(testOne, testTwo)
+	diffLen := len(diff)
+	if diffLen != 2 {
+		t.Fatal("Basic Int8 Test failed.")
+	}
 }
-
-
